e41fd43ffdbc6e5f2429c43bb27c4e44e4488705
kokorechextraction <cocaobreak@gmail.ru>
2025-07-30 23:27:32 +0300

    added registerview class

--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView, RedirectView
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth import login
 
-# Create your views here.
+
+class RegisterView(FormView):
+    template_name = "user_app/register.html"
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
+
