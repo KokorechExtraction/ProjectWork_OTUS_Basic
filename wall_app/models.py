fc2911f7bc8d9421965e78905d46e5de590e0b96
kokorechextraction <cocaobreak@gmail.ru>
2025-09-01 21:02:34 +0300

    model added in wall_app

--- wall_app/models.py
+++ wall_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from user_app.models import CustomUser
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey("Author", on_delete=models.CASCADE, related_name="post")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    tags = models.ManyToManyField('Tag', related_name='posts')
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.author
+
+class Comment(models.Model):
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return f"{self.author}'s comment"
+
+
+class AuthorProfile(models.Model):
+    author = models.OneToOneField("CustomUser", on_delete=models.CASCADE, related_name="profile")
+
+    def __str__(self):
+        return self.author
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
