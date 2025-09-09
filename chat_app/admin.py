c1123e2bc593cf6d4550b6e936114f7eb834eecc
kokorechextraction <cocaobreak@gmail.ru>
2025-09-09 19:27:01 +0300

    chat_app admin updated

--- chat_app/admin.py
+++ chat_app/admin.py
@@ -5,12 +5,12 @@ from chat_app.models import Message
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ("sender", "recipient", "body", "created_at",)
+    list_display = ("sender", "recipient", "body",)
 
     ordering = ("sender", "recipient", "created_at",)
     list_filter = ("sender", "recipient", "created_at",)
     search_fields = ("sender", "recipient", "created_at",)
     search_help_text = "Введите слово для поиска"
 
-    fields = ("sender", "recipient", "body", "created_at",)
+    fields = ("sender", "recipient", "body",)
 
